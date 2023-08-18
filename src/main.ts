643b9928f24413e46e70d33ab370e85d8d157099
Nicolas <asternic@gmail.com>
2023-08-18 19:57:44 -0300

    Update main.ts. Add type guard for new typescript version.

--- src/main.ts
+++ src/main.ts
@@ -112,7 +112,9 @@ async function run() {
 
 
   } catch (error) {
-    core.setFailed(error.message);
+      if (error instanceof Error) {
+          core.setFailed(error.message);
+      }
   }
 }
 
