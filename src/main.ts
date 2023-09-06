e87c7291342f28b70faa929224094266fb06ae3c
Nicolas <asternic@gmail.com>
2023-09-06 20:21:01 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -61,7 +61,7 @@ async function run() {
     // Copy tar.gz file to source path
     await exec.exec(`cp ${name}-${version}.tar.gz /github/home/rpmbuild/SOURCES/`);
 
-    await exec.exec(`yum-builddep -y ${specFile}`);
+    //await exec.exec(`yum-builddep -y ${specFile}`);
 
     // Execute rpmbuild , -ba generates both RPMS and SPRMS
     try {
