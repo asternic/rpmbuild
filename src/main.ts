9cefd97b93331d82e7f93a09acbb5c613e958dca
Nicolas <asternic@gmail.com>
2023-09-06 22:03:52 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -45,7 +45,8 @@ async function run() {
     await exec.exec(`find /github/workspace`);
     await exec.exec(`find /github/home/rpmbuild`);
     //await exec.exec(`cp /github/workspace/build/5.0/patches/* /github/home/rpmbuild/SOURCES/`);
-    await exec.exec(`cp /github/workspace/build/5.0/files/* /github/home/rpmbuild/SOURCES/`);
+    await exec.exec(`find /github/workspace/build/5.0/files -type f -exec cp {} /github/home/rpmbuild/SOURCES/ ;`);
+    await exec.exec(`find /github/workspace/build/5.0/patches -type f -exec cp {} /github/home/rpmbuild/SOURCES/ ;`);
 
     // Dowload tar.gz file of source code,  Reference : https://developer.github.com/v3/repos/contents/#get-archive-link
     await exec.exec(`curl -L --output tmp.tar.gz https://api.github.com/repos/${owner}/${repo}/tarball/${ref}`)
